cc390a417aca24880129b2c0f266a5f7f96ecec3
谢家明 <xiejiaming@ydjdev.com>
2022-11-29 15:43:22 +0800

    更改打包

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "/yuntong/",
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
